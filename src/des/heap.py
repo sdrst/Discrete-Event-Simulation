386f1824b8a141619030a994033f54bc4b7a6fb9
Sam Durst <sam.durst@affirm.com>
2026-07-09 13:58:49 -0400

    what was i thinking

--- src/des/heap.py
+++ src/des/heap.py
@@ -1,36 +1,36 @@
 """Binary min-heap operations over a fixed-size list of Event objects, keyed on event_time."""
 
 
-def siftup(Heap, i):
+def siftup(heap, i):
     while i > 0:
         p = (i - 1) // 2
 
-        if Heap[p].event_time < Heap[i].event_time and Heap[p].event_time != 0.0:
-            return Heap
+        if heap[p].event_time < heap[i].event_time and heap[p].event_time != 0.0:
+            return heap
 
-        Heap[i], Heap[p] = Heap[p], Heap[i]
+        heap[i], heap[p] = heap[p], heap[i]
         i = p
-        siftup(Heap, i)
-    return Heap
+        siftup(heap, i)
+    return heap
 
 
-def siftdown(Heap, i):
-    end = len(Heap) - 1
+def siftdown(heap, i):
+    end = len(heap) - 1
     while True:
         child = i * 2 + 1
         if child > end:
             break
-        if child + 1 <= end and Heap[child].event_time > Heap[child + 1].event_time and Heap[child + 1].event_time != 0.0:
+        if child + 1 <= end and heap[child].event_time > heap[child + 1].event_time and heap[child + 1].event_time != 0.0:
             child += 1
 
-        if Heap[i].event_time > Heap[child].event_time and Heap[child].event_time != 0.0:
-            Heap[i], Heap[child] = Heap[child], Heap[i]
+        if heap[i].event_time > heap[child].event_time and heap[child].event_time != 0.0:
+            heap[i], heap[child] = heap[child], heap[i]
             i = child
-            siftdown(Heap, i)
+            siftdown(heap, i)
 
         else:
             break
-    return Heap
+    return heap
 
 
 def printf(heap, i):
